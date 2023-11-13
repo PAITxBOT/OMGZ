754de3bedc81fdfb2ae6536cb5d2d2525b08d939
harixtg <150332613+harixtg@users.noreply.github.com>
2023-11-13 10:13:05 +0530

    Add files via upload

--- update.py
+++ update.py
@@ -1,16 +1,17 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ
+from os import path as ospath, environ, execl as osexecl
 from subprocess import run as srun
 from requests import get as rget
 from dotenv import load_dotenv
+from sys import executable
 from pymongo import MongoClient
 
-if ospath.exists('Z_Logs.txt'):
-    with open('Z_Logs.txt', 'r+') as f:
+if ospath.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
         f.truncate(0)
 
 basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
-                    handlers=[FileHandler('Z_Logs.txt'), StreamHandler()], level=INFO)
+                    handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
 
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
@@ -30,13 +31,6 @@ except:
 
 load_dotenv('config.env', override=True)
 
-try:
-    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
-        log_error('The README.md file there to read! Exiting now!')
-        exit()
-except:
-    pass
-
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
@@ -50,7 +44,7 @@ if len(DATABASE_URL) == 0:
 
 if DATABASE_URL:
     conn = MongoClient(DATABASE_URL)
-    db = conn.z
+    db = conn.luna
     if config_dict := db.settings.config.find_one({'_id': bot_id}):
         environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
         environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
@@ -58,18 +52,18 @@ if DATABASE_URL:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://gitlab.com/Dawn-India/Z-Mirror'
+    UPSTREAM_REPO = 'https://github.com/5hojib/Aeon'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'upstream'
+    UPSTREAM_BRANCH = 'main'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email omk.xd@telegram.com \
-                 && git config --global user.name omkzz \
+                 && git config --global user.email yesiamshojib@gmail.com \
+                 && git config --global user.name 5hojib \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
@@ -77,10 +71,6 @@ update = srun([f"git init -q \
                  && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
 if update.returncode == 0:
-    log_info('Successfully updated with latest commit.')
-    log_info(f'Repo in use: {UPSTREAM_REPO}')
-    log_info(f'Branch in use: {UPSTREAM_BRANCH}')
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
-    log_error('Something went wrong while updating.')
-    log_info('Check if entered UPSTREAM_REPO is valid or not!')
-    log_info(f'Entered upstream repo: {UPSTREAM_REPO}')
+    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
