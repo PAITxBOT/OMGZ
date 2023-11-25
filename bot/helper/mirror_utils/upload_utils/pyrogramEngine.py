d5cbb1a94a4002cdbfbbeb7ca610b2acebada280
HarixTGz <151769010+HarixTGz@users.noreply.github.com>
2023-11-25 13:35:08 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -118,8 +118,8 @@ class TgUploader:
   
     async def __prepare_file(self, file_, dirpath):
         if self.__lprefix or self.__lremname:
-            dfile_ = await remove_unwanted(file_, self.__lremname)
-            file_ = f"<b>{self.__lprefix} {dfile_}</b>"
+            file_ = await remove_unwanted(file_, self.__lremname)
+            #file_ = f"<b>{self.__lprefix} {dfile_}</b>"
             cap_mono = f"<b>{self.__lprefix} {file_}</b>"
             self.__lprefix = re_sub('<.*?>', '', self.__lprefix)
             if self.__listener.seed and not self.__listener.newDir and not dirpath.endswith("/splited_files_z"):
